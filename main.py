3e490dd8be0189a9890b46b532d7365ae3c8c52f
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-01 00:25:50 +0530

    update

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from scipy.io.wavfile import write
 import wavio as wv
 
 freq=44100
-duration=7
+duration=int(input("Enter the duration of audio in seconds:"))
 
 record=sd.rec(int(duration*freq), samplerate=freq,channels=2)
 
 sd.wait()
 
-file_name1=input("Enter the filename:")
-file_name2=input("Enter the filename:")
+# file_name1=input("Enter the filename:")
+file_name=input("Enter the filename:")
 
 
-write(f"C:/Users/lenovo/Desktop/{file_name1}",freq,record) #scipy.io.wavfile
+# write(f"C:/Users/lenovo/Desktop/{file_name1}",freq,record) #scipy.io.wavfile
 
-wv.write(f"C:/Users/lenovo/Desktop/{file_name2}",record,freq,sampwidth=2) #using wavio
+wv.write(f"C:/Users/lenovo/Desktop/{file_name}",record,freq,sampwidth=2) #using wavio
 
 

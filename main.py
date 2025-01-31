728cdcadbdddf3b1027bb60f7de16cf3358e4da2
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-31 15:06:48 +0530

    saving files

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ duration=7
 
 record=sd.rec(int(duration*freq), samplerate=freq,channels=2)
 sd.wait()
+
+write("recording0",freq,record) #scipy.io.wavfile
+
+wv.write("recording1",record,freq,sampwidth=2)
+
+
